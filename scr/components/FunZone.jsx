845613eb677d01d8e39093edbba13e2270333f39
tiwooluwaseyi2008 <tiwooluwaseyi2008@gmail.com>
2026-04-15 13:16:25 +0100

    Create FunZone.jsx

---
+++ scr/components/FunZone.jsx
@@ -0,0 +1,6 @@
+// src/components/FunZone.jsx  
+import React from 'react';  
+function FunZone() {  
+  return <div className="container mt-3"><h3>Fun Zone</h3><p>Games, quizzes and more coming soon!</p></div>;  
+}  
+export default FunZone;  
